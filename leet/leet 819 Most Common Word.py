0a084b18fa3195eb2e7d6432e507e24ad40ddbbd
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-12-04 19:56:20 +0900

    파일 이동

--- leet/leet 819 Most Common Word.py
+++ leet/leet 819 Most Common Word.py
@@ -9,13 +9,3 @@ class Solution:
                 frequency[word] += 1
 
         return max(frequency, key=frequency.get)
-#
-# import collections
-#
-# class Solution:
-#     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-#         parse_str: [str] = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
-#
-#         counts = collections.Counter(parse_str);
-#
-#         return counts.most_common(1)[0][0]
