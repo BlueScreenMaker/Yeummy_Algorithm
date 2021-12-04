7cbf86550b4d48809f18d71ef7a143e4d589fedb
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-12-04 22:24:00 +0900

    힙 샘플 코드

--- 힙/simpleHeap.py
+++ 힙/simpleHeap.py
@@ -18,6 +18,28 @@ class Heap:
         else:
             return False
 
+    def move_down(self, poped_idx):
+        # 왼쪽 자식 인덱스
+        left_child_idx = poped_idx * 2
+        # 오른쪽 자식 인덱스
+        right_child_idx = poped_idx * 2 + 1
+
+        # 왼쪽 자식 인덱스 유효성 체크
+        if left_child_idx >= len(self.heap_array):
+            return False
+
+        # 오른쪽 자식 인덱스 유효성 체크 
+        elif right_child_idx >= len(self.heap_array):
+            # 오른쪽 자식이 유효하지 않다면, 그냥 바로 왼쪽 자식으로 스왑
+            if self.heap_array[poped_idx] < self.heap_array[left_child_idx]:
+                return True
+        # 해당 부모 노드에 두 개의 자식 노드를 가질 때
+        else:
+            # 부모보다 자식 노드가 큰 것이 있을 때
+            if self.heap_array[poped_idx] < self.heap_array[left_child_idx] or self.heap_array[poped_idx] < self.heap_array[right_child_idx]:
+                return True
+        return False
+
     def insert(self, data):
         if len(self.heap_array) == 0:
             self.heap_array.append(None)
@@ -37,3 +59,49 @@ class Heap:
             inserted_idx = parent_idx
 
         return True
+
+    def pop(self):
+        if len(self.heap_array) <= 1:
+            return None
+        
+        returned_data = self.heap_array[1]
+
+        self.heap_array[1] = self.heap_array[-1]
+
+        del self.heap_array[-1]
+
+        poped_idx = 1
+
+        while self.move_down(poped_idx):
+            left_child_idx = poped_idx * 2
+            right_child_idx = poped_idx * 2 + 1
+
+            # 오른쪽 인덱스가 유효하지 않고, 왼쪽 인덱스의 값이 부모의 값보다 클 때
+            if right_child_idx >= len(self.heap_array) and self.heap_array[poped_idx] < self.heap_array[left_child_idx]:
+                self.heap_array[left_child_idx], self.heap_array[poped_idx] = self.heap_array[poped_idx], self.heap_array[left_child_idx]
+
+                poped_idx = left_child_idx
+                return
+            
+            # 부모 노드가 두 개의 노드를 가지고 있고, 이 자식 노드가 부모의 값보다 클 때
+            if self.heap_array[poped_idx] < self.heap_array[left_child_idx] or self.heap_array[poped_idx] < self.heap_array[right_child_idx]:
+                max_idx = right_child_idx if self.heap_array[left_child_idx] < self.heap_array[right_child_idx] else left_child_idx
+
+                self.heap_array[max_idx], self.heap_array[poped_idx] = self.heap_array[poped_idx], self.heap_array[max_idx]
+
+                poped_idx = max_idx
+                return
+            
+        return returned_data
+    
+
+heap = Heap(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+heap.insert(20)
+print(heap.heap_array)
+
+heap.pop()
+print(heap.heap_array)
