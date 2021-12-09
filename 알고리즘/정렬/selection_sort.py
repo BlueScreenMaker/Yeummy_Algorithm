3b046eec558540793479d98cb0dd7e13c4483464
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-12-09 15:54:15 +0900

    선택 정렬

---
+++ 알고리즘/정렬/selection_sort.py
@@ -0,0 +1,19 @@
+import random
+
+def selection_sort(data):
+
+    for i in range(len(data) - 1): 
+        target: int = i
+        for j in range(i + 1, len(data)):
+            if data[target] > data[j]:
+                target = j
+                
+        if target != i:
+            data[i], data[target] = data[target], data[i]
+
+    return data
+
+data_list: list = random.sample(range(100), 50)
+
+print(selection_sort(data_list))
+
