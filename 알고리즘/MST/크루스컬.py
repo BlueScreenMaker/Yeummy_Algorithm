3234ddaf6f196439b615601e604a120f2c06fb9f
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-01-24 23:43:58 +0900

    크루스컬

--- 알고리즘/MST/크루스컬.py
+++ 알고리즘/MST/크루스컬.py
@@ -25,27 +25,32 @@ mygraph= {
     ]
 }
 
+# "~의 노드의 부모는 ~이다"를 나타내는 것
 parent = {}
+
 rank = {}
+
+def make_set(node):
+    # 초기에는 아무것도 연결 안되어 있으니, 내가 부모 노드가 됨
+    parent[node] = node
+    # 랭크를 0
+    rank[node] = 0
 
 def find(node):
     # path compression 기법
-    if parent[node] != node:
-        parent[node] = find(parent[node])
+    if parent[node] != node: # 만약, 나의 부모가 내가 아니면 (즉, 내가 부모를 가지고 있을 때)
+        parent[node] = find(parent[node]) # 나의 루트 노드를 반환 ( 즉, 최종적인 루트 노드로만 연결하기 위해 )
     return parent[node]
         
-
-def make_set(node):
-    parent[node] = node
-    rank[node] = 0
 
 def union(node_v, node_u):
     root1 = find(node_v)
     root2 = find(node_u)
-    
+    # 랭크가 다르다면, 랭크가 더 큰놈에 작은놈을 붙임
     if rank[root1] > rank[root2]:
         parent[root2] = root1
     else:
+        # 일단 붙이고 랭크를 높여준다.
         parent[root1] = root2
         if rank[root1] == rank[root2]:
             rank[root2] += 1
@@ -66,9 +71,10 @@ def kruskal(graph):
         weight, node_v, node_u = edge
         
         # 이 두개의 노드의 루트노드가 다를때 (같으면 이 두개를 선택하면 안됨.)
+        # 왜냐하면 싸이클이 생기니까!
         if find(node_v) != find(node_u):
-            union(node_v, node_u) # union을 통해서 합친다.
-            mst.append(edge)
+            union(node_v, node_u) # union을 두개의 노드를 하나의 집합으로 합친다.
+            mst.append(edge) # 정상이라면 해당 간선을 푸쉬
     return mst
             
 print(kruskal(mygraph))
