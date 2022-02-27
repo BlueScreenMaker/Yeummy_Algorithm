bee51cf7f644ec39b037a529d7632cf07399c6d8
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-27 09:58:26 +0900

    BOJ 9663 N-Queen

---
+++ BOJ/BOJ 9663 N-Queen.js
@@ -0,0 +1,34 @@
+const N = Number(require('fs').readFileSync(__dirname + '/dev/stdin').toString().trim());
+
+let answer = 0;
+
+function isAvailable(candidate, currentCol) {
+    const currentRow = candidate.length;
+
+    for (let i = 0; i < candidate.length; i++) {
+        if (candidate[i] === currentCol ||
+            Math.abs(candidate[i] - currentCol) === currentRow - i) 
+            return false
+    }
+
+    return true;
+}
+
+function DFS(N, currentRow, curCandidate) {
+    if (currentRow === N) {
+        answer += 1;
+        return;
+    }
+
+    for (let i = 0; i < N; i++) {
+        if (isAvailable(curCandidate, i)) {
+            curCandidate.push(i);
+            DFS(N, currentRow + 1, curCandidate);
+            curCandidate.pop();
+        }
+    }
+}
+
+DFS(N, 0, []);
+
+console.log(answer);
