0ef05c1d8bb7263bb4a5294127c689b9ebbf758a
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-03-11 23:11:27 +0900

    숫자 문자열과 영단어

---
+++ Programmers/카카오/숫자 문자열과 영단어.js
@@ -0,0 +1,21 @@
+function solution(s) {
+
+    s = s.replace(/zero/g, 0);
+    s = s.replace(/one/g, 1);
+    s = s.replace(/two/g, 2);
+    s = s.replace(/three/g, 3);
+    s = s.replace(/four/g, 4);
+    s = s.replace(/five/g, 5);
+    s = s.replace(/six/g, 6);
+    s = s.replace(/seven/g, 7);
+    s = s.replace(/eight/g, 8);
+    s = s.replace(/nine/g, 9);
+
+    return Number(s);
+}
+
+console.log(solution("one4seveneight") === 1478);
+console.log(solution("23four5six7") === 234567);
+console.log(solution("2three45sixseven") === 234567);
+console.log(solution("123") === 123);
+
