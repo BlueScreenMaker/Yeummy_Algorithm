2c9ef41152185949d744f583cb1eefef8179df69
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-07-22 17:29:12 +0900

    프린터

--- Programmers/고득점 kit/스택, 큐/프린터.js
+++ Programmers/고득점 kit/스택, 큐/프린터.js
@@ -1,25 +1,44 @@
+/**
+ * 일반적인 프린터는 인쇄 요청이 들어온 순서대로 인쇄한다.
+ *
+ * 그렇기 때문에 중요한 문서가 나중에 인쇄될 수 있다.
+ *
+ * 이런 문제를 보완하기 위해 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다.
+ *
+ * 이 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
+ *
+ * 1. 인쇄 대기목록의 가장 앞에 있는 문서 J를 대기 목록에서 꺼낸다.
+ * 2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재한다면, J를 대기 목록의 가장 마지막에 넣는다.
+ * 3. 그렇지 않으면 J를 인쇄한다.
+ *
+ *
+ */
+
 function solution(priorities, location) {
-    let answer = 0;
-    const schedules = priorities.map((priority, index) => [priority, index]);
+  let locString = null;
+  const CHAR_A = 65;
 
-    for (let i = 0; i < schedules.length; i++) {
-        const maxValue = [...schedules.slice(i)].sort((a, b) => b[0] - a[0])[0][0];
-        const [priority, index] = schedules[i];
+  const data = priorities.map((v, i) => {
+    const char = String.fromCharCode(CHAR_A + i);
+    if (i === location) locString = char;
 
-        if (priority < maxValue) {
-            schedules.splice(i, 1);
-            schedules.push([priority, index]);
-            i -= 1;
-        }
-    }
+    return [char, v];
+  });
 
-    schedules.forEach((schedule, index) => {
-        if (schedule[1] === location)
-            answer = index;
-    })
+  for (let i = 0; i < data.length; i++) {
+    const [_, value] = data[i];
+    const isMaxPriority = data.slice(i + 1).every(([, v]) => v <= value);
 
-    return answer + 1;
+    if (isMaxPriority) continue;
+
+    data.push(data.splice(i, 1)[0]);
+    i--;
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] === locString) return i + 1;
+  }
 }
 
-solution([2, 1, 3, 2], 2);
-solution([1, 1, 9, 1, 1, 1], 0);
+console.log(solution([2, 1, 3, 2], 2));
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
